638b451c423f1ddefe0876d3e90c3da83e4187c8
yurugalaxy <theblueberrywolfe@gmail.com>
2024-07-27 22:33:29 +0100

    add error handling for user input

--- src/player.cpp
+++ src/player.cpp
@@ -2,12 +2,45 @@
 
 #include "player.hpp"
 
+void ignoreLine()
+{
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),  '\n');
+}
+
+bool clearFailedExtraction()
+{
+        if (!std::cin)
+        {
+                if(std::cin.eof())
+                {
+                        exit(0);
+                }
+
+                std::cin.clear();
+                ignoreLine();
+
+                return true;
+        }
+        return false;
+}
+
 int Player::TakeTurn()
 {
-        std::cout << "Choose a square: ";
-        int square{};
-        std::cin >> square;
-        return square;
+        while(true)
+        {
+                std::cout << "Choose a square: ";
+                int square{};
+                std::cin >> square;
+
+                if (clearFailedExtraction() || square < 1 || square > 9)
+                {
+                        std::cout << "Invalid input, try again.\n";
+                        continue;
+                }
+
+                ignoreLine();
+                return square;
+        }
 }
 
 char Player::GetSide()
