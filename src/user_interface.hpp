ed5d086a5c6de6b416e2ca414bcbc30086038b65
yurugalaxy <theblueberrywolfe@gmail.com>
2024-08-05 16:29:42 +0000

    add user interface files

---
+++ src/user_interface.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "tictactoe.hpp"
+
+void clear();
+void createMessage(std::string message);
+void appendMessage(std::string message);
+void printLogo();
+void printBoard(Game& game);
+void draw();
